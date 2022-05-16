4ca5faa23dc0acfd704e6ee499ee8ba1c6ee5e96
tsainei yuan <hi@tsainei.com>
2022-05-16 15:53:34 +0200

    finish challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,8 @@
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
-    if params[:question].end_with?("?")
+    if params[:question].end_with?('?')
       @answer = 'Silly question, get dressed and go to work!'
     elsif params[:question] == 'I am going to work right now!'
       @answer = 'Great'
